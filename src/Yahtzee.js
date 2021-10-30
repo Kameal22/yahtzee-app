9cbfac42e5f6dd3544c00f4032c5aff12f0715bc
Kamil Czyżewski <kamil.czyzewski96@gmail.com>
2021-10-30 12:13:14 +0200

    Main styles set

---
+++ src/Yahtzee.js
@@ -0,0 +1,13 @@
+import './gameStyles/Yahtzee.css'
+import {Component} from 'react'
+
+class Yahtzee extends Component{
+
+  render(){
+    return(
+      <div className = "Yahtzee"></div>
+    )
+  }
+}
+
+export default Yahtzee;
