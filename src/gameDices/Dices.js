e964af1bb59cc7bd4367db95b05b5fc45f191994
kamil <kami.czyzewski96@gmail.com>
2021-11-01 19:25:14 +0100

    Added game start option

--- src/gameDices/Dices.js
+++ src/gameDices/Dices.js
@@ -14,34 +14,51 @@ class Dices extends Component{
     super(props)
     this.state = {
       dices : [
-        {face : 6, id : 1, isChosen : false},
-        {face : 6, id : 2, isChosen : false},
-        {face : 6, id : 3, isChosen : false},
-        {face : 6, id : 4, isChosen : false},
-        {face : 6, id : 5, isChosen : false},
+        {face : 1, id : 1, isChosen : false},
+        {face : 2, id : 2, isChosen : false},
+        {face : 3, id : 3, isChosen : false},
+        {face : 4, id : 4, isChosen : false},
+        {face : 5, id : 5, isChosen : false},
         {face : 6, id : 6, isChosen : false}
       ],
-      numRolls : 3,
-      countPointsTime : false
+      numRolls : 2,
+      countPointsTime : false,
+      gameStart : false
     }
   }
 
-// RANDOMIZE ONLY DICES THAT AREN'T CHOSEN !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-  randomizeDices = () =>{
+  entryRandomize = () =>{
     const randDices = this.state.dices.map(dice =>{
       let randInt = Math.floor(Math.random() * this.props.dieFaces.length) +1
-      return {...dice, face : randInt}
+      if(dice.isChosen === false){
+        return {...dice, face : randInt}
+      }
+      return dice
     })
+    this.setState({
+      dices : randDices,
+    })
+  }
+
+  randomizeDices = () =>{
     if(this.state.numRolls !== 0){
+    const randDices = this.state.dices.map(dice =>{
+      let randInt = Math.floor(Math.random() * this.props.dieFaces.length) +1
+      if(dice.isChosen === false){
+        return {...dice, face : randInt}
+      }
+      // Create scd array of chosen dices to summarize their score values
+      return dice
+    })
     this.setState(currLimit =>({
       dices : randDices,
       numRolls : currLimit.numRolls -1
     }))
-  }else{
-    this.setState({
-      countPointsTime : true
-    })
+    if(this.state.numRolls === 1){
+      this.setState({
+        countPointsTime : true
+      })
+    }
   }}
 
   chooseDice = (id) =>{
@@ -58,6 +75,7 @@ class Dices extends Component{
   }
 
   render(){
+    if(this.state.gameStart === true){
     return(
       <div className = "DieSection">
         <h1>Yahtzee!</h1>
@@ -78,6 +96,29 @@ class Dices extends Component{
         <button onClick = {this.randomizeDices} >{`${this.state.numRolls} Roll left!`}</button>
       </div>
     )
+    }else{
+      return(
+        <div className = "DieSection">
+        <h1>Yahtzee!</h1>
+        <div className = "DieDiv">
+          
+        {this.state.dices.map(dice =>{
+          return <Die
+            face = {dice.face}
+            key = {dice.id}
+            id = {dice.id}
+            isChosen = {dice.isChosen}
+            choose = {this.chooseDice}
+            rollLimit = {this.setRollLimit}
+            />
+        })}
+
+        </div>
+        <button onClick = {() =>{this.setState({gameStart : true}); this.entryRandomize();}}
+         >Start the game!</button>
+      </div>
+      )
+    }
   }
 }
 
